41e040c6d535c67b4ccfb1cc5919a81199058cc7
Chloe Atchue-Mamlet <chloe793@gmail.com>
2015-09-16 16:40:32 -0400

    safari notice

--- js/functions.js
+++ js/functions.js
@@ -8,3 +8,9 @@ if (isTouchDevice() === true) {
     //alert('Not a Touch Device'); //your logic for non touch device here
     $('#portfolio-index').addClass('project-grid');
 }
+
+
+if (navigator.userAgent.search("Safari") >= 0 && navigator.userAgent.search("Chrome") < 0) 
+{
+   alert("Oops, this is embarrassing! My website currently does not look its best when viewed with Safari. It's a problem I'm aware of, and I'm working on fixing it. You can still proceed, but I recommend using Chrome instead for now.");          
+}
