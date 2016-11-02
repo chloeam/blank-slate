840b831174b3d80c25e851d367b6cb7c5941aec5
Chloe Atchue-Mamlet <chloe793@gmail.com>
2016-11-01 23:56:28 -0400

    trying to undo changes

--- js/functions.js
+++ js/functions.js
@@ -1,10 +1,11 @@
-// function isTouchDevice() {
-//     return true == ("ontouchstart" in window || window.DocumentTouch && document instanceof DocumentTouch);
-// }
-// if (isTouchDevice() === true) {
-//     //alert('Touch Device'); //your logic for touch device here
-//     $('#portfolio-index').addClass('touch');
-// } else {
-//     //alert('Not a Touch Device'); //your logic for non touch device here
-//     $('#portfolio-index').addClass('project-grid');
-// }
+function isTouchDevice() {
+    return true == ("ontouchstart" in window || window.DocumentTouch && document instanceof DocumentTouch);
+}
+if (isTouchDevice() === true) {
+    //alert('Touch Device'); //your logic for touch device here
+    $('#portfolio-index').addClass('touch');
+} else {
+    //alert('Not a Touch Device'); //your logic for non touch device here
+    $('#portfolio-index').addClass('project-grid');
+}
+
